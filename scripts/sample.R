e0745a3ae0c550ae8b902b88237baeb5c33bf7fe
Angela Yang <137425902+kirbysgf@users.noreply.github.com>
2023-06-22 12:37:10 -0400

    Update sample.R by adding AY initials

--- scripts/sample.R
+++ scripts/sample.R
@@ -1,5 +1,5 @@
 # Create the vector
-char_vector <- c("LO", "MD", "SP", )
+char_vector <- c("LO", "MD", "SP", "AY", )
 
 # Randomly sort the vector
 sorted_vector <- sample(char_vector, size = length(char_vector))
