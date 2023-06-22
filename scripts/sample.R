59629d62fac3506c9e1ac64ba5e1017eae423268
rmtrivedi <137425727+rmtrivedi@users.noreply.github.com>
2023-06-22 12:40:03 -0400

    Update sample.R by adding "RT"

--- scripts/sample.R
+++ scripts/sample.R
@@ -1,5 +1,5 @@
 # Create the vector
-char_vector <- c("LO", "MD", "SP", "AY","SD", "EE","SL" )
+char_vector <- c("LO", "MD", "SP", "AY","SD", "EE","SL","RT" )
 
 # Randomly sort the vector
 sorted_vector <- sample(char_vector, size = length(char_vector))
